09bacc8be078f056795fce0c9622074b2371e9a4
Gazimjanov Egor <Egar.net2000@gmail.com>
2025-05-19 17:17:15 +0600

    Add ex1

---
+++ ex1/ArraySort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ArraySort<T>
+    {
+
+        public void SortArray<T>(T[] arr)
+        {
+            Console.WriteLine("Сортировка массива");
+            Array.Sort(arr);
+            Console.WriteLine(string.Join(", ", arr));
+        }
+
+        public void ChangeOrder<T>(T[] arr) 
+        {
+            Console.WriteLine("Изменения порядка на обратный");
+            Array.Reverse(arr);
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
---
+++ ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = {1, 4 ,9 ,0};
+
+            ArraySort<int> sort = new ArraySort<int>();
+            Console.WriteLine(string.Join(", ", mas));
+            sort.SortArray(mas);
+            sort.ChangeOrder(mas);
+        }
+    }
+}
